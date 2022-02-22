ce425b8a36dbde1c20883b0f30330508c7f5f6c0
lybbn <yanwei@qq.com>
2022-02-22 13:50:04 +0800

    删除无用文件

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -40,7 +40,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
