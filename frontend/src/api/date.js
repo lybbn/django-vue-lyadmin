398c2da22f9e7b9b0191a459fc3d543cf8669f0c
lybbn <yanwei@qq.com>
2022-06-29 18:02:45 +0800

    新增时间显示特殊方法

--- frontend/src/api/date.js
+++ frontend/src/api/date.js
@@ -1,4 +1,49 @@
 // date.js
+export function specialDatetime(dateTimeStamp) {
+  dateTimeStamp=new Date(dateTimeStamp).getTime();
+    var minute = 1000 * 60;      //把分，时，天，周，半个月，一个月用毫秒表示
+    var result;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var week = day * 7;
+    var halfamonth = day * 15;
+    var month = day * 30;
+    var now = new Date().getTime();   //获取当前时间毫秒
+    var diffValue = now - dateTimeStamp;//时间差
+    if(diffValue < 0){
+        return;
+    }
+    var minC = diffValue/minute;  //计算时间差的分，时，天，周，月
+    var hourC = diffValue/hour;
+    var dayC = diffValue/day;
+    var weekC = diffValue/week;
+    var monthC = diffValue/month;
+    if(monthC >= 1 && monthC <= 3){
+        result = parseInt(monthC) + "月前"
+    }else if(weekC >= 1 && weekC <= 3){
+        result = parseInt(weekC) + "周前"
+    }else if(dayC >= 1 && dayC <= 6){
+        result = parseInt(dayC) + "天前"
+    }else if(hourC >= 1 && hourC <= 23){
+        result = parseInt(hourC) + "小时前"
+    }else if(minC >= 1 && minC <= 59){
+        result =parseInt(minC) + "分钟前"
+    }else if(diffValue >= 0 && diffValue <= minute){
+        result = "刚刚"
+    }else {
+        var datetime = new Date();
+        datetime.setTime(dateTimeStamp);
+        var Nyear = datetime.getFullYear();
+        var Nmonth = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+        var Ndate = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+        var Nhour = datetime.getHours() < 10 ? "0" + datetime.getHours() : datetime.getHours();
+        var Nminute = datetime.getMinutes() < 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+        var Nsecond = datetime.getSeconds() < 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+        result = Nyear + "-" + Nmonth + "-" + Ndate
+
+    }
+    return result;
+}
 export function formatDate(date, fmt) {
   if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
